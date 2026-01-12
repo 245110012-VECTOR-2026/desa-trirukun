5b1aa0f3e5b3c8a17caeb86a65ee5d49788095cf
245110012-VECTOR-2026 <neylarahmadani2024.student@umitra.ac.id>
2026-01-11 22:39:39 -0800

    Add files via upload

---
+++ data/point_3.js
@@ -0,0 +1,10 @@
+var json_point_3 = {
+"type": "FeatureCollection",
+"name": "point_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 98.723625188857213, 3.599263472689688 ] } },
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "Point", "coordinates": [ 98.724217176844974, 3.599027144534038 ] } },
+{ "type": "Feature", "properties": { "id": "12" }, "geometry": { "type": "Point", "coordinates": [ 122.457644288920832, 0.724536078191213 ] } }
+]
+}
